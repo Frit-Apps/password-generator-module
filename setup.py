23d6de463f7c6d4f403a0a03e0bc61b2c7edf21b
Mazzya <yahyamazouarenna@gmail.com>
2021-06-08 10:44:25 +0200

    Variable Version updated

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 with open('README.md', 'r', encoding='utf-8') as file:
     long_description = file.read()
